8248451293c72ad02a9b3af83fc1166cf67eea04
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-13 12:58:35 -0700

    Added rush hour pricing to set_base_price method

--- melons.py
+++ melons.py
@@ -1,14 +1,32 @@
 """Classes for melon orders."""
 
+from datetime import datetime
+from random import randint
+
 class AbstractMelonOrder(object):
     def __init__(self, species, qty):
         self.species = species
         self.qty = qty
         self.shipped = False
-        self.base_price = 5
+        self.set_base_price()
 
+
+        
+    def set_base_price(self):
+        """add splurge pricing to base price"""
+
+        #base_price is a random int between 5 and 9
+        self.base_price = randint(5, 9)
+        now = datetime.now()
+
+        #christmas melons have a higher cost of 1.5 that of other melons
         if self.species == 'christmas melon':
             self.base_price = 1.5 * self.base_price
+
+
+        #prices during rush hour (weekdays between 8-11) have an added $5 fee
+        if now.weekday() < 5 and now.hour < 11 and now.hour >= 8:
+            self.base_price += 4
 
 
     def get_total(self):
